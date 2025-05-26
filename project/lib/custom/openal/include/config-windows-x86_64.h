fcf95e444d9b3d6e146277828c8a9f09456cae66
Rapper GF <84131849+RapperGF@users.noreply.github.com>
2025-05-26 17:39:53 -0400

    Update config-windows-x86_64.h
    openal is driving me crazy.

--- project/lib/custom/openal/include/config-windows-x86_64.h
+++ project/lib/custom/openal/include/config-windows-x86_64.h
@@ -3,7 +3,7 @@
 #define ALC_API __declspec(dllexport)
 
 /* Define a restrict macro for non-aliased pointers */
- /* #define RESTRICT __restrict */
+#define RESTRICT __restrict
 
 /* Define if HRTF data is embedded in the library */
 #define ALSOFT_EMBED_HRTF_DATA
